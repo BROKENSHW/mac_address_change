d4d7b86576850a611a9fdc54686e0d52aa635660
BROKENSHW <102567706+BROKENSHW@users.noreply.github.com>
2024-09-15 22:06:49 -0400

    Delete defChangeMAC.py

--- defChangeMAC.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import subprocess
-
-#Função para alterar o MAC - altera o mac com base no argumento do user
-def change_mac(interface, new_mac):
-    print(f"[+] Changing MAC Address for {interface} to {new_mac}")
-    subprocess.call(["ifconfig", interface, "down"])
-    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
-    subprocess.call(["ifconfig", interface, "up"])
